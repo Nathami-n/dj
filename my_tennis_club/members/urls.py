25624b5cc499e959a5439bfc4a28c1d1ed1f6593
Nathami-n <georgenathan010@gmail.com>
2024-07-24 14:39:46 +0300

    Add: the automatic loading of data

--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("members/", views.members, name="members")
+    path("members/", views.members, name="members"),
+    path("details/<int:id>", views.details, name="details"),
+    path("", views.home, name="home")
 ]
--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,6 +1,7 @@
 
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 from .models import Member
 
 
@@ -12,3 +13,13 @@ def members(request):
         "mymembers": my_members,
     }
     return HttpResponse(template.render(context, request))
+
+
+def home(request):
+    template  = loader.get_template("home.html")
+    return render(request, "home.html")
+
+
+def details(request, id):
+    mymember = Member.objects.get(id=id)
+    return render(request, "details.html", {"mymember": mymember})
